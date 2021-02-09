56dfb11472602f03310c38c1a5aa833ab3e4627d
adamis <adamisstarling@gmail.com>
2021-02-09 17:26:05 -0300

    bug fix

--- src/main/java/br/com/esquadro/helper/GenericHelperInterface.java
+++ src/main/java/br/com/esquadro/helper/GenericHelperInterface.java
@@ -4,5 +4,5 @@ import java.util.List;
 import java.util.Map;
 
 public interface GenericHelperInterface {
-	public List<Map> executeSQL(String query);	
+	public List<Map> executeSQL(String query) throws Exception;	
 }
--- src/main/java/br/com/esquadro/helper/OracleHelper.java
+++ src/main/java/br/com/esquadro/helper/OracleHelper.java
@@ -3,7 +3,6 @@ package br.com.esquadro.helper;
 import java.util.List;
 import java.util.Map;
 
-import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.DB;
 
 import br.com.esquadro.sqlite.entity.BancoDadosEntity;
@@ -14,24 +13,32 @@ public class OracleHelper implements GenericHelperInterface {
 	private static DB open;
 	
 	public OracleHelper(BancoDadosEntity bancoDados) {
-		this.bancoDados = bancoDados;		
+		this.bancoDados = bancoDados;
+		try {
+			if(open != null) {
+				close();
+			}
+			open = open();
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public List<Map> executeSQL(String query) {
-		System.err.println("Query: "+query);
-		if(open == null) {
-			open = open();
-		}		
-		List<Map> findAll = open.findAll(query);		
-		return findAll;
+	public List<Map> executeSQL(String query) throws Exception {
+		System.err.println("Query: "+query);				
+		//List<Map> findAll = open.findAll(query);		
+		return open.findAll(query);
 	}
 
-	private DB open() {
-		return Base.open(
+	private DB open() throws Exception {
+		DB temp = new DB("oracle");
+				temp.open(
 				"oracle.jdbc.OracleDriver", "jdbc:oracle:thin:@" + bancoDados.getIp() + ":" + bancoDados.getPorta()
 				+ "/" + bancoDados.getNameBd() ,//+ "?oracle.jdbc.timezoneAsRegion=false",
 				bancoDados.getUsuario(), bancoDados.getSenha());
+				
+		return temp;
 	}
 	
 	private void close() {
